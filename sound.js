7753782bce0924be20f3eec919304cdd5e79269b
naluaaaaa <163869108+naluaaaaa@users.noreply.github.com>
2024-07-01 12:47:19 -0300

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,77 @@
+var a = new Audio();
+a.src = "a.mp3"
+
+var b = new Audio();
+b.src = "b.mp3"
+
+var c = new Audio();
+c.src = "c.mp3"
+
+var d = new Audio();
+d.src = "d.mp3"
+
+var e = new Audio();
+e.src = "e.mp3"
+
+var f = new Audio();
+f.src = "f.mp3"
+
+var g = new Audio();
+g.src = "g.mp3"
+
+var h = new Audio();
+h.src = "h.mp3"
+
+var i = new Audio();
+i.src = "i.mp3"
+
+var j = new Audio();
+j.src = "j.mp3"
+
+var k = new Audio();
+k.src = "k.mp3"
+
+var l = new Audio();
+l.src = "l.mp3"
+
+var m = new Audio();
+m.src = "m.mp3"
+
+var n = new Audio();
+n.src = "n.mp3"
+
+var o = new Audio();
+o.src = "o.mp3"
+
+var p = new Audio();
+p.src = "p.mp3"
+
+var q = new Audio();
+q.src = "q.mp3"
+
+var r = new Audio();
+r.src = "r.mp3"
+
+var s = new Audio();
+s.src = "s.mp3"
+
+var t = new Audio();
+t.src = "t.mp3"
+
+var u = new Audio();
+u.src = "u.mp3"
+
+var v = new Audio();
+v.src = "v.mp3"
+
+var w = new Audio();
+w.src = "w.mp3"
+
+var x = new Audio();
+x.src = "x.mp3"
+
+var y = new Audio();
+y.src = "y.mp3"
+
+var z = new Audio();
+z.src = "z.mp3"
